0f5c4a337da0a3b64aa484faa657bcf1ddb3234a
Klimchuk Daniil <klimchuk.daniil@gmail.com>
2025-09-03 10:18:16 +0300

    Small update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
 @file:Suppress("UnstableApiUsage")
 
-plugins { `kotlin-dsl` }
+plugins {
+    `kotlin-dsl`
+}
 
 dependencies {
-  implementation(libs.android.gradlePlugin)
-  implementation(libs.detekt.gradlePlugin)
-  implementation(libs.dokka.gradlePlugin)
-  implementation(libs.kotlin.gradlePlugin)
-  implementation(libs.mavenPublishPlugin)
-  implementation(libs.spotless.gradlePlugin)
+    implementation(libs.android.gradlePlugin)
+    implementation(libs.detekt.gradlePlugin)
+    implementation(libs.dokka.gradlePlugin)
+    implementation(libs.kotlin.gradlePlugin)
+    implementation(libs.mavenPublishPlugin)
+    implementation(libs.spotless.gradlePlugin)
 }
